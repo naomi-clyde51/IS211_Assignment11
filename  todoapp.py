3dcbe239aaa03a1c4259ca22c6709e632dd22de4
Naomi Clyde <49344161+naomi-clyde51@users.noreply.github.com>
2026-04-26 16:25:54 -0400

    Pass tasks to index template in todoapp.py

---  todoapp.py
+++  todoapp.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# Tasks
+tasks = ['Buy groceries', 'Walk the dog', 'Make dinner']
+
 # Defining homepage
 @app.route('/') # Defines single route to homepage
 def index():
-  return render_template 'index.html' # Renders HTML template
+  return render_template ('index.html', tasks=tasks) # Renders HTML template
 
 if __name__ == '__main__':
   app.run(debug=True) 
