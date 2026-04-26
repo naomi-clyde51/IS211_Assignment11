e63d1ba42b8c1f5bc65ac748cff316fb8187376b
Naomi Clyde <49344161+naomi-clyde51@users.noreply.github.com>
2026-04-26 16:16:15 -0400

    Set up Flask app with homepage route

---  todoapp.py
+++  todoapp.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
+# Defining homepage
+@app.route('/') # Defines single route to homepage
+def index():
+  return render_template 'index.html' # Renders HTML template
+
+if __name__ == '__main__':
+  app.run(debug=True) 
